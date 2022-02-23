ec62067008fd9c785e38be089806286f83df8919
김예나 <87893678+kyn1013@users.noreply.github.com>
2022-02-24 01:03:22 +0900

    Create 10951 A+B - 4.cpp

---
+++ BOJ/10951 A+B - 4.cpp
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a(0), b(0);
+	while (cin>>a) {
+		cin >> b;
+		cout << a + b<<"\n";
+	}
+	return 0;
+}
