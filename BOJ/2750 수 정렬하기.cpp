bbffbadba6d9cb52a03caef91e3405430cfcd733
김예나 <87893678+kyn1013@users.noreply.github.com>
2023-11-09 10:26:15 +0900

    Create 2750 수 정렬하기.cpp

---
+++ BOJ/2750 수 정렬하기.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    int arr[1000] = {};
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    sort(arr, arr + N);
+    for (int i = 0; i < N; i++) cout << arr[i] << "\n";
+}
